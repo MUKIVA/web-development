960dd72aef260b81d6862a63bfabd3fbb736ab59
Иван Мухортов <mukiva2261@gmail.com>
2020-04-25 15:33:52 +0300

    Fix Lw8

--- Lw8/js/scripts.js
+++ Lw8/js/scripts.js
@@ -1,59 +1,65 @@
-const div = document.querySelector('.film');
-const elem = div.getElementsByTagName('div');
-const on_screen = 4;
-const all_films = 10;
-var j = 0;
-//let i = 0;
-
-function right(event) {
-  if (j >= 0) {
-    elem[j].classList.remove('film1');
-    elem[j].classList.add('hidden_block');
-  } else {
-    elem[j + all_films].classList.remove('film1');
-    elem[j + all_films].classList.add('hidden_block');
-  }
-  for (let i = 1; i <= on_screen; i++) {
-    if (i + j >= 0) {
-      elem[i+j].classList.remove('film'+(i+1));
-      elem[i+j].classList.add('film'+i);
-      elem[i+j].classList.remove('hidden_block');
-    } else {
-      elem[all_films+i+j].classList.remove('film'+(i+1));
-      elem[all_films+i+j].classList.add('film'+i);
-      elem[all_films+i+j].classList.remove('hidden_block');
-    }
-  }
-  if (j == 5) {
-    j = -4;
-  } else {
-    j++;
-  }
-};
-
-function left(event) {
-  elem[j + on_screen - 1].classList.remove('film4');
-  elem[j + on_screen - 1].classList.add('hidden_block');
-  for (let i = 3; i >= 0; i--) {
-    if (i + j > 0) {
-      elem[i + j - 1].classList.remove('film' + i);
-      elem[i + j - 1].classList.add('film' + (i + 1));
-      elem[i + j - 1].classList.remove('hidden_block');
-    } else {
-      elem[i + all_films + j - 1].classList.remove('film' + i);
-      elem[i + all_films + j - 1].classList.add('film' + (i + 1));
-      elem[i + all_films + j - 1].classList.remove('hidden_block');
-    }
-  }
-  if (j == -3) {
-    j = 6;
-  } else {
-    j--;
-  }
-}
-
-
 function run() {
+    const divOfFilms = document.querySelector('.film');
+    const elemOfDiv = divOfFilms.getElementsByTagName('div');
+    const on_screen = 4;
+    const all_films = 10;
+    let condition = 0;
+
+
+    function offset(conditionValue, buttonDirection, filmLength){
+      if (condition == 6 && buttonDirection == "right") {
+        condition = condition - filmLength;
+      } else if (condition == -4 && buttonDirection == "left") {
+        condition = condition + filmLength;
+      }
+      return condition;
+    }
+
+
+    function right() {
+      condition = offset(condition, "right", elemOfDiv.length);
+      if (condition >= 0) {
+        elemOfDiv[condition].classList.remove('film1');
+        elemOfDiv[condition].classList.add('hidden_block');
+      } else {
+        elemOfDiv[condition + all_films].classList.remove('film1');
+        elemOfDiv[condition + all_films].classList.add('hidden_block');
+      }
+      for (let i = 1; i <= on_screen; i++) {
+        if (i + condition >= 0) {
+          elemOfDiv[i+condition].classList.remove('film'+(i+1));
+          elemOfDiv[i+condition].classList.add('film'+i);
+          elemOfDiv[i+condition].classList.remove('hidden_block');
+        } else {
+          elemOfDiv[all_films+i+condition].classList.remove('film'+(i+1));
+          elemOfDiv[all_films+i+condition].classList.add('film'+i);
+          elemOfDiv[all_films+i+condition].classList.remove('hidden_block');
+        }
+      }
+      condition++;
+      console.log(condition)
+    };
+    console.log(elemOfDiv.length - condition);
+
+    function left() {
+      condition = offset(condition, "left", elemOfDiv.length);
+      elemOfDiv[condition + on_screen - 1].classList.remove('film4');
+      elemOfDiv[condition + on_screen - 1].classList.add('hidden_block');
+      for (let i = 3; i >= 0; i--) {
+        if (i + condition > 0) {
+          elemOfDiv[i + condition - 1].classList.remove('film' + i);
+          elemOfDiv[i + condition - 1].classList.add('film' + (i + 1));
+          elemOfDiv[i + condition - 1].classList.remove('hidden_block');
+        } else {
+          elemOfDiv[i + all_films + condition - 1].classList.remove('film' + i);
+          elemOfDiv[i + all_films + condition - 1].classList.add('film' + (i + 1));
+          elemOfDiv[i + all_films + condition - 1].classList.remove('hidden_block');
+        }
+      }
+      condition--;
+      console.log(condition)
+    }
+
     const rightArrow = document.getElementById('right_event');
     rightArrow.addEventListener('click', right);
 
